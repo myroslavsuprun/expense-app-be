3e69c3bb7822a328a257655ea28738df47bda670
myroslavsuprun <myroslavsuprun@gmail.com>
2025-05-17 15:18:09 +0300

    feat: update relations with cascade

---
+++ src/database/prisma/migrations/20250517121744_transaction_category_optional/migration.sql
@@ -0,0 +1,26 @@
+-- DropForeignKey
+ALTER TABLE "audit_logs" DROP CONSTRAINT "audit_logs_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "categories" DROP CONSTRAINT "categories_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "transactions" DROP CONSTRAINT "transactions_category_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "transactions" DROP CONSTRAINT "transactions_user_id_fkey";
+
+-- AlterTable
+ALTER TABLE "transactions" ALTER COLUMN "category_id" DROP NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "categories" ADD CONSTRAINT "categories_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "audit_logs" ADD CONSTRAINT "audit_logs_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
